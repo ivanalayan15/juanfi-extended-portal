6c9ac76eb5872ad1af98877fb10874aa803ec988
Rio Cadeliña <cadelina13@gmail.com>
2026-03-28 22:00:36 +0800

    fix pause time

--- assets/js/core.js
+++ assets/js/core.js
@@ -1186,6 +1186,7 @@ function renderView() {
                 $("#rewardDtlsBtn").addClass("hide");
                 $("#resumeBtnContainer").addClass("hide");
                 $("#pauseBtnContainer").addClass("hide");
+                $("#pauseBtnContainer").addClass("hide");
                 $("#rateTable").addClass("hide");
                 $("#chargingBtn").addClass("hide");
                 $("#eloadBtn").addClass("hide");
@@ -1288,6 +1289,7 @@ function multiVendoConfiguration(vendo, user) {
     } else {
         $("#insertBtnContainer").removeClass("hide");
     }
+    showPauseTime = vendo.showPauseTime;
     if (!vendo.showPauseTime) {
         $("#pauseBtnContainer").addClass("hide");
     } else {
